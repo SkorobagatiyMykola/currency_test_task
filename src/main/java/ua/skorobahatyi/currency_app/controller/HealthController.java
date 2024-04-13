4d763c44c31a88325648b05c9a411ffbd99aac57
SkorobagatiyMykola <nick1980457@gmail.com>
2024-04-13 22:11:07 +0300

    Updated HealthController

--- src/main/java/ua/skorobahatyi/currency_app/controller/HealthController.java
+++ src/main/java/ua/skorobahatyi/currency_app/controller/HealthController.java
@@ -1,6 +1,7 @@
 package ua.skorobahatyi.currency_app.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/v1/test")
 public class HealthController {
-    @GetMapping(produces = "application/json")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> checkApp(){
         String message ="{\"status\": \"UP\", \"mood\": \"cool\",\"code\":200}";
 
