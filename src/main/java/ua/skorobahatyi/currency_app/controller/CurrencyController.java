eb4eb28e03e0faeb7c7f54c8365e590c4596a127
SkorobagatiyMykola <nick1980457@gmail.com>
2024-04-14 16:08:02 +0300

    Updated CurrencyController logic

--- src/main/java/ua/skorobahatyi/currency_app/controller/CurrencyController.java
+++ src/main/java/ua/skorobahatyi/currency_app/controller/CurrencyController.java
@@ -9,25 +9,22 @@ import org.springframework.web.bind.annotation.*;
 import ua.skorobahatyi.currency_app.entity.dto.CurrencyResponseDto;
 import ua.skorobahatyi.currency_app.entity.dto.DeleteMessage;
 import ua.skorobahatyi.currency_app.exception.GenericSystemException;
-import ua.skorobahatyi.currency_app.exception.IncorrectDateFormate;
 import ua.skorobahatyi.currency_app.service.CurrencyService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/currencies")
 public class CurrencyController {
     private static final Logger logger = LogManager.getLogger(CurrencyController.class);
-
     private final CurrencyService currencyService;
-    private static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     @GetMapping("")
-    public ResponseEntity<CurrencyResponseDto> getActualRateCurrency() throws GenericSystemException {
-        LocalDate dateNow = LocalDate.now();
+    public ResponseEntity<CurrencyResponseDto> getActualCurrencyRates() throws GenericSystemException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String dateNow = formatter.format(LocalDate.now());
         logger.debug("=== GET currency rates on the actual date: {}", dateNow);
         CurrencyResponseDto response = currencyService.findCurrencyRatesByDate(dateNow);
 
@@ -35,30 +32,20 @@ public class CurrencyController {
     }
 
     @GetMapping("/{dateId}")
-    public ResponseEntity<CurrencyResponseDto> getRateCurrencyToDate(@PathVariable("dateId") String dateId) throws GenericSystemException {
-        LocalDate dateReport = checkAndGetCorrectDate(dateId);
-        logger.debug("=== GET currency rates on the date: {}", dateReport);
+    public ResponseEntity<CurrencyResponseDto> getCurrencyRatesOnDate(@PathVariable("dateId") String dateStr) throws GenericSystemException {
+      //  LocalDate dateReport = checkAndGetCorrectDate(dateId);
+        logger.debug("=== GET currency rates on the date: {}", dateStr);
 
-        CurrencyResponseDto response = currencyService.findCurrencyRatesByDate(dateReport);
+        CurrencyResponseDto response = currencyService.findCurrencyRatesByDate(dateStr);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{dateId}")
-    public ResponseEntity<DeleteMessage> deleteRateCurrencyByDate(@PathVariable("dateId") String dateId) throws GenericSystemException{
-        LocalDate dateReport = checkAndGetCorrectDate(dateId);
-        logger.debug("=== GET currency rates on the date: {}", dateReport);
-        DeleteMessage response = currencyService.deleteCurrencyRatesByDate(dateReport);
+    public ResponseEntity<DeleteMessage> deleteRateCurrencyOnDate(@PathVariable("dateId") String dateStr) throws GenericSystemException {
+        logger.debug("=== GET currency rates on the date: {}", dateStr);
+        DeleteMessage response = currencyService.deleteCurrencyRatesByDate(dateStr);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    private static LocalDate checkAndGetCorrectDate(String dateId) throws IncorrectDateFormate {
-        try {
-            return LocalDate.parse(dateId,FORMATTER);
-        } catch (DateTimeParseException ex) {
-            String message = "Incorrect date format, to get a correct answer, the date must be in this format: dd-mm-yyyy";
-            throw new IncorrectDateFormate(HttpStatus.BAD_REQUEST, message);
-        }
     }
 }
